97b6afeff6d991b64c27a981acdf8f3095343131
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-01-09 13:32:31 -0300

    v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados';
+  dadoL = "../assets/img/dice1.png";
+  dadoR = "../assets/img/dice5.png";
+  num1:number;
+  num2:number;
+  resultado:string;
+  ganaste:boolean=false;
+  contador:number=0;
+
+  tirarDados():void{
+    this.contador++;
+    this.num1 = Math.floor(Math.random() * 6) + 1;
+    this.num2 = Math.floor(Math.random() * 6) + 1;
+    this.dadoL = "../assets/img/dice" + this.num1 + ".png";
+    this.dadoR = "../assets/img/dice" + this.num2 + ".png";
+    if(this.validarDados()){
+      this.resultado = (this.contador===1)?"Ganaste en 1 intento":"Ganaste en "+this.contador+" intentos"; 
+      this.ganaste = true;
+      //SetTimeout para que se vea el mensaje de ganaste antes de que se reinicie el juego
+      setTimeout(() => {
+        this.reiniciar();
+      }, 2000);
+    }
+  }
+  validarDados():boolean{
+    if(this.num1 == this.num2){
+      return true;
+    }
+    return false;
+  }
+  reiniciar():void{
+    this.dadoL = "../assets/img/dice1.png";
+    this.dadoR = "../assets/img/dice5.png";
+    this.contador = 0;
+    this.ganaste = false;
+    this.resultado = "";
+  }
 }
